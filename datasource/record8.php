22b23e37fc36fb526e38660da81d67e9cd1c0527
Keyfish <98537753+Keyfish@users.noreply.github.com>
2022-02-01 07:33:13 -0500

    Initial commit

---
+++ datasource/record8.php
@@ -0,0 +1,18 @@
+<?php
+
+// This recordset is for the third picture
+
+
+$content = array  // an array is a set of related information. Ex: multiple pieces of info about one photo
+  (
+array  // the third array inside "content": child array #3
+(
+      'id'                => "8",
+      'metatitle'         => "Beachwood Stump",
+      'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+      'h1title'           => "Beachwood Stump at Cap Bon Ami",
+      'imagepath'         => "/img/image-8.jpg",
+      'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Québec Canada",
+      'copyright'         => "&copy Jeremy F. Nadeau, 2021")
+);
+?>  
---
+++ datasource/recordset-thumbnails.php
@@ -0,0 +1,180 @@
+<?php
+
+// This recordset is for the THUMBNAILS GALLERY page
+
+$content = array (  // an array is a set of related information. Ex: multiple pieces of info about one photo
+                    // $content is the "parent" array, as it holds several other arrays
+
+array (   // the first array inside "content":: child array #1
+		'id'                => "1",
+    'metatitle'         => "Icelandic landscape",
+    'metadescription'   => "Jokulsarlon glacial lagoon",
+		'h1title'           => "Jokulsarlon glacial lagoon",
+		'imagepath'         => "/img/thumbs/thumbnail-1.jpg",
+		'caption'           => "Icebergs in the Jokulsarlon glacial lagoon on the south coast of Iceland",
+    'copyright'         => "&copy Jeremy F. Nadeau, 2014"),  // last line ends with a ),
+
+  array (  // the second array inside "content": child array #2
+      'id'                => "2",
+      'metatitle'         => "Jordanian temple",
+      'metadescription'   => "Jordanian temple in Petra",
+      'h1title'           => "Jordanian temple in Petra",
+      'imagepath'         => "/img/thumbs/thumbnail-2.jpg",
+      'caption'           => "The Treasury: a Jordanian temple located in Petra",
+      'copyright'         => "&copy Jeremy F. Nadeau, 2013"),
+
+    array (  // the third array inside "content": child array #3
+      'id'                => "3",
+      'metatitle'         => "Beachwood Stump",
+      'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+      'h1title'           => "Beachwood Stump at Cap Bon Ami",
+      'imagepath'         => "/img/thumbs/thumbnail-3.jpg",
+      'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+      'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+
+ array (
+'id'                => "4",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-4.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "5",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-5.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "6",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-6.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "7",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-7.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "8",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-8.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "9",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-9.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),    
+
+array (
+'id'                => "11",
+'metatitle'         => "Beachwood Stump",
+'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+'h1title'           => "Beachwood Stump at Cap Bon Ami",
+'imagepath'         => "/img/thumbs/thumbnail-11.jpg",
+'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+array (
+  'id'                => "12",
+  'metatitle'         => "Beachwood Stump",
+  'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+  'h1title'           => "Beachwood Stump at Cap Bon Ami",
+  'imagepath'         => "/img/thumbs/thumbnail-12.jpg",
+  'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+  'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+  array (
+    'id'                => "13",
+    'metatitle'         => "Beachwood Stump",
+    'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+    'h1title'           => "Beachwood Stump at Cap Bon Ami",
+    'imagepath'         => "/img/thumbs/thumbnail-13.jpg",
+    'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+    'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+    array (
+      'id'                => "14",
+      'metatitle'         => "Beachwood Stump",
+      'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+      'h1title'           => "Beachwood Stump at Cap Bon Ami",
+      'imagepath'         => "/img/thumbs/thumbnail-14.jpg",
+      'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+      'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+      array (
+        'id'                => "15",
+        'metatitle'         => "Beachwood Stump",
+        'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+        'h1title'           => "Beachwood Stump at Cap Bon Ami",
+        'imagepath'         => "/img/thumbs/thumbnail-15.jpg",
+        'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+        'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+        array (
+          'id'                => "16",
+          'metatitle'         => "Beachwood Stump",
+          'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+          'h1title'           => "Beachwood Stump at Cap Bon Ami",
+          'imagepath'         => "/img/thumbs/thumbnail-16.jpg",
+          'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+          'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+          array (
+            'id'                => "17",
+            'metatitle'         => "Beachwood Stump",
+            'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+            'h1title'           => "Beachwood Stump at Cap Bon Ami",
+            'imagepath'         => "/img/thumbs/thumbnail-17.jpg",
+            'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+            'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+            array (
+              'id'                => "18",
+              'metatitle'         => "Beachwood Stump",
+              'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+              'h1title'           => "Beachwood Stump at Cap Bon Ami",
+              'imagepath'         => "/img/thumbs/thumbnail-18.jpg",
+              'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+              'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+              array (
+                'id'                => "19",
+                'metatitle'         => "Beachwood Stump",
+                'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+                'h1title'           => "Beachwood Stump at Cap Bon Ami",
+                'imagepath'         => "/img/thumbs/thumbnail-10.jpg",
+                'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+                'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+
+                array (
+                  'id'                => "20",
+                  'metatitle'         => "Beachwood Stump",
+                  'metadescription'   => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie",
+                  'h1title'           => "Beachwood Stump at Cap Bon Ami",
+                  'imagepath'         => "/img/thumbs/thumbnail-20.jpg",
+                  'caption'           => "Beachwood Stump at Cap Bon Ami Beach in Gaspésie Queébec Canada",
+                  'copyright'         => "&copy Jeremy F. Nadeau, 2021"),
+);
+?>
